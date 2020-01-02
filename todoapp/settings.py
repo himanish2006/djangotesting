867eaefb262a5f21c651f1a2d54eaa704f6c1d29
himanish2006 <himanish200@gmail.com>
2020-01-02 20:33:03 +0530

    I can not migrate

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
+        'NAME': 'todoapp',
         'USER': 'blog_admin',
         'PASSWORD': 'testing123',
         'HOST': 'localhost',
