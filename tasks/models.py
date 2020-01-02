9481475178e17971eefc9c014ef6f7795a0edaf9
himanish2006 <himanish200@gmail.com>
2020-01-02 17:04:36 +0530

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class ToDo(models.Model):
+    sr_no =models.AutoField(primary_key=True)
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True,blank =True)
+
+    def __str__(self):
+        return self.content
+
+# Create your models here.
